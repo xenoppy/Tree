cf6530f58f4da40f259a34a8d7a710a6cea42a43
xenoppy <1340319151@qq.com>
2021-06-18 09:59:24 +0800

    进一步精简

--- algorithm.cpp
+++ algorithm.cpp
@@ -3,78 +3,95 @@
 #include"kill.h"
 #include"func.h"
 #include"convention.h"
+void initialize(int* sum1, int* sum2, int* minus1, int* minus2) {
+	*minus1 = 1;
+	*minus2 = 1;
+	*sum1 = 0;
+	*sum2 = 0;
+
+}
+bool checkwin(int* sum1,int* sum2,int* temp) {
+	if (*sum1 == 5) {
+		*temp = -10000000000000;
+		return true;
+	}
+	else if (*sum2 == 5) {
+		*temp = 10000000000000;
+		return true;
+	}
+}
+void empty(int* sum1, int* sum2, int* minus1, int* minus2, int* score, int* temp, int* a) {
+	if (*sum1 - *minus1 > -1)*score = *score - 1.1 * a[sum1 - minus1];
+	if (*sum2 - *minus2 > -1)*score = *score + a[sum2 - minus2];
+
+}
+void emptyback(int* sum1, int* sum2, int* minus1, int* minus2, int* score, int* temp, int* a) {
+
+	*sum1 = 0;
+	*sum2 = 0;
+	*minus1 = 0;
+	*minus2 = 0;
+}
+
+void white(int* sum1, int* sum2, int* minus1, int* minus2, int* score, int* temp, int* a) {
+	*sum1 = *sum1 + 1;
+	*minus2 = *minus2 + 1;
+	if (*sum2 - *minus2 > -1)*score = *score + a[sum2 - minus2];
+}
+void whiteback(int* sum1, int* sum2, int* minus1, int* minus2, int* score, int* temp, int* a) {
+
+	*sum2 = 0;
+	*minus2 = 1;
+}
+
+void black(int* sum1, int* sum2, int* minus1, int* minus2, int* score, int* temp, int* a) {
+	*minus1 = *minus1 + 1;
+	*sum2 = *sum2 + 1;
+	if (*sum1 - *minus1 > -1)*score = *score - 1.1 * a[sum1 - minus1];
+
+}
+void blackback(int* sum1, int* sum2, int* minus1, int* minus2, int* score, int* temp, int* a) {
+
+
+	*sum1 = 0;
+	*minus1 = 1;
+}
 void evaluate(int** board,int user,int x,int y,int* temp) {
 	int sum1,sum2,minus1,minus2,score=0,a[6] = { 1,10,100,1000,1000000,10000000000000 };
-
+	initialize(&sum1, &sum2, &minus1, &minus2);
 	//横向
 	for (int i = 0; i < BOARD_LENTH; i++) {
-		minus1 = 1;
-		minus2 = 1;
-		sum1 = 0;
-		sum2 = 0;
+		
 		for (int j = 0; j < BOARD_LENTH; j++) {
 			if (board[i][j] == EMPTY) {
-				if (sum1 - minus1 > -1)score = score - 1.1 * a[sum1 - minus1];
-				if (sum2 - minus2 > -1)score = score + a[sum2 - minus2];
-				//检查是否胜利
-				if (sum1 == 5) {
-					*temp = -10000000000000;
-					return;
-				}
-				else if (sum2 == 5) {
-					*temp = 10000000000000;
-					return;
-				}
-				sum1 = 0;
-				sum2 = 0;
-				minus1 = 0;
-				minus2 = 0;
+				empty(&sum1, &sum2, &minus1, &minus2, &score, temp, a);
+				if (checkwin(&sum1, &sum2,  temp))return;
+				emptyback(&sum1, &sum2, &minus1, &minus2, &score, temp, a);
 			}
-			else if (board[i][j] == 0) {
-				sum1 = sum1 + 1;
-				minus2 = minus2 + 1;
-				if (sum2 - minus2 > -1)score = score + a[sum2 - minus2];
+			else if (board[i][j] == WHITE) {
+				white(&sum1, &sum2, &minus1, &minus2, &score, temp, a);
+				//检查是否胜利
+				if (checkwin(&sum1, &sum2,  temp))return;
+				whiteback(&sum1, &sum2, &minus1, &minus2, &score, temp, a);
 				
-				//检查是否胜利
-				if (sum1 == 5) {
-					*temp = -10000000000000;
-					return;
-				}
-				else if (sum2 == 5) {
-					*temp = 10000000000000;
-					return;
-				}
-				sum2 = 0;
-				minus2 = 1;
+	
+				
 			}
-			else {
-				minus1 = minus1 + 1;
-				sum2 = sum2 + 1;
-				if (sum1 - minus1 > -1)score = score - 1.1 * a[sum1 - minus1];
+			else {//黑棋				
+				black(&sum1, &sum2, &minus1, &minus2, &score, temp, a);
 				//检查是否胜利
-				if (sum1 == 5) {
-					*temp = -10000000000000;
-					return;
-				}
-				else if (sum2 == 5) {
-					*temp = 10000000000000;
-					return;
-				}
-				sum1 = 0;
-				minus1 = 1;
+				if (checkwin(&sum1, &sum2,  temp))return;
+				blackback(&sum1, &sum2, &minus1, &minus2, &score, temp, a);
+
 			}
 		}
-		minus1 = minus1 + 1;
-		minus2 = minus2 + 1;
-		if (sum1 == 5) {
-			*temp = -10000000000000;
+		//抵达边界
+		minus1 ++ ;
+		minus2 ++ ;
+		if (checkwin(&sum1, &sum2, temp)) {
 			return;
 		}
-		else if (sum2 == 5) {
-			*temp = 10000000000000;
-			return;
-		}
-		else {
+		else {//结账
 			if (sum1 - minus1 > -1)score = score - 1.1 * a[sum1 - minus1];
 			if (sum2 - minus2 > -1)score = score + a[sum2 - minus2];
 		}
@@ -82,27 +99,12 @@ void evaluate(int** board,int user,int x,int y,int* temp) {
 	
 	//纵向
 	for (int j = 0; j < BOARD_LENTH; j++) {
-		minus1 = 1;
-		minus2 = 1;
-		sum1 = 0;
-		sum2 = 0;
+		initialize(&sum1, &sum2, &minus1, &minus2);
 		for (int i = 0; i < BOARD_LENTH; i++) {
 			if (board[i][j] == EMPTY) {
-				if (sum1 - minus1 > -1)score = score - 1.1 * a[sum1 - minus1];
-				if (sum2 - minus2 > -1)score = score + a[sum2 - minus2];
-				//检查是否胜利
-				if (sum1 == 5) {
-					*temp = -10000000000000;
-					return;
-				}
-				else if (sum2 == 5) {
-					*temp = 10000000000000;
-					return;
-				}
-				sum1 = 0;
-				sum2 = 0;
-				minus1 = 0;
-				minus2 = 0;
+				empty(&sum1, &sum2, &minus1, &minus2, &score, temp, a);
+				if (checkwin(&sum1, &sum2, temp))return;
+				emptyback(&sum1, &sum2, &minus1, &minus2, &score, temp, a);
 			}
 			else if (board[i][j] == 0) {
 				sum1 = sum1 + 1;
@@ -138,95 +140,53 @@ void evaluate(int** board,int user,int x,int y,int* temp) {
 				minus1 = 1;
 			}
 		}
-		minus1 = minus1 + 1;
-		minus2 = minus2 + 1;
-		if (sum1 == 5) {
-			*temp = -10000000000000;
+		//抵达边界
+		minus1++;
+		minus2++;
+		if (checkwin(&sum1, &sum2, temp)) {
 			return;
 		}
-		else if (sum2 == 5) {
-			*temp = 10000000000000;
-			return;
-		}
-		else {
-			if (sum1 - minus1 > -1)score = score - a[sum1 - minus1];
+		else {//结账
+			if (sum1 - minus1 > -1)score = score - 1.1 * a[sum1 - minus1];
 			if (sum2 - minus2 > -1)score = score + a[sum2 - minus2];
 		}
 	}
 	
 	//左下右上
 	for (int i = 0; i < BOARD_LENTH * 2 - 1; i++) {
+		initialize(&sum1, &sum2, &minus1, &minus2);
 		int j = 0;
-		minus1 = 1;
-		minus2 = 1;
-		sum1 = 0;
-		sum2 = 0;
+		
 
 		while (j >= 0 && i - j >= 0 && j < BOARD_LENTH && i - j < BOARD_LENTH) {
 			if (board[j][i - j] == EMPTY) {
-				if (sum1 - minus1 > -1)score = score - a[sum1 - minus1];
-				if (sum2 - minus2 > -1)score = score + a[sum2 - minus2];
-				//检查是否胜利
-				if (sum1 == 5) {
-					*temp = -10000000000000;
-					return;
-				}
-				else if (sum2 == 5) {
-					*temp = 10000000000000;
-					return;
-				}
-				sum1 = 0;
-				sum2 = 0;
-				minus1 = 0;
-				minus2 = 0;
+				empty(&sum1, &sum2, &minus1, &minus2, &score, temp, a);
+				if (checkwin(&sum1, &sum2,  temp))return;
+				emptyback(&sum1, &sum2, &minus1, &minus2, &score, temp, a);
 			}
-			else if (board[j][i - j] == 0) {
-				sum1 = sum1 + 1;
-				minus2 = minus2 + 1;
-				if (sum2 - minus2 > -1)score = score + a[sum2 - minus2];
+			else if (board[j][i - j] == WHITE) {
+				white(&sum1, &sum2, &minus1, &minus2, &score, temp, a);
+				//检查是否胜利
+				if (checkwin(&sum1, &sum2, temp))return;
+				whiteback(&sum1, &sum2, &minus1, &minus2, &score, temp, a);
 
-				//检查是否胜利
-				if (sum1 == 5) {
-					*temp = -10000000000000;
-					return;
-				}
-				else if (sum2 == 5) {
-					*temp = 10000000000000;
-					return;
-				}
-				sum2 = 0;
-				minus2 = 1;
 			}
-			else {
-				minus1 = minus1 + 1;
-				sum2 = sum2 + 1;
-				if (sum1 - minus1 > -1)score = score - 1.1 * a[sum1 - minus1];
+			else {//黑棋								
+				black(&sum1, &sum2, &minus1, &minus2, &score, temp, a);
 				//检查是否胜利
-				if (sum1 == 5) {
-					*temp = -10000000000000;
-					return;
-				}
-				else if (sum2 == 5) {
-					*temp = 10000000000000;
-					return;
-				}
-				sum1 = 0;
-				minus1 = 1;
+				if (checkwin(&sum1, &sum2,  temp))return;
+				blackback(&sum1, &sum2, &minus1, &minus2, &score, temp, a);
+
 			}
 			j++;
 		}
-		//检查最后边界
-		minus1 = minus1 + 1;
-		minus2 = minus2 + 1;
-		if (sum1 == 5) {
-			*temp = -10000000000000;
+		//抵达边界
+		minus1++;
+		minus2++;
+		if (checkwin(&sum1, &sum2, temp)) {
 			return;
 		}
-		else if (sum2 == 5) {
-			*temp = 10000000000000;
-			return;
-		}
-		else {
+		else {//结账
 			if (sum1 - minus1 > -1)score = score - 1.1 * a[sum1 - minus1];
 			if (sum2 - minus2 > -1)score = score + a[sum2 - minus2];
 		}
@@ -234,152 +194,76 @@ void evaluate(int** board,int user,int x,int y,int* temp) {
 
 
 	//左上右下
+	//该情况需要用两个while去遍历所有路径
 	for (int i = 1 - BOARD_LENTH; i <= BOARD_LENTH - 1; i++) {
+		initialize(&sum1, &sum2, &minus1, &minus2);
 		int j = 0;
-		minus1 = 1;
-		minus2 = 1;
-		sum1 = 0;
-		sum2 = 0;
-
 		while (i + j >= 0 && j < BOARD_LENTH && j + i < BOARD_LENTH) {
 			if (board[j][i + j] == EMPTY) {
-				if (sum1 - minus1 > -1)score = score - a[sum1 - minus1];
-				if (sum2 - minus2 > -1)score = score + a[sum2 - minus2];
-				//检查是否胜利
-				if (sum1 == 5) {
-					*temp = -10000000000000;
-					return;
-				}
-				else if (sum2 == 5) {
-					*temp = 10000000000000;
-					return;
-				}
-				sum1 = 0;
-				sum2 = 0;
-				minus1 = 0;
-				minus2 = 0;
+				empty(&sum1, &sum2, &minus1, &minus2, &score, temp, a);
+				if (checkwin(&sum1, &sum2,  temp))return;
+				emptyback(&sum1, &sum2, &minus1, &minus2, &score, temp, a);
 			}
-			else if (board[j][i + j] == 0) {
-				sum1 = sum1 + 1;
-				minus2 = minus2 + 1;
-				if (sum2 - minus2 > -1)score = score + a[sum2 - minus2];
-
+			else if (board[j][i + j] == WHITE) {
+				white(&sum1, &sum2, &minus1, &minus2, &score, temp, a);
 				//检查是否胜利
-				if (sum1 == 5) {
-					*temp = -10000000000000;
-					return;
-				}
-				else if (sum2 == 5) {
-					*temp = 10000000000000;
-					return;
-				}
-				sum2 = 0;
-				minus2 = 1;
+				if (checkwin(&sum1, &sum2,  temp))return;
+				whiteback(&sum1, &sum2, &minus1, &minus2, &score, temp, a);
+
 			}
 			else {
-				minus1 = minus1 + 1;
-				sum2 = sum2 + 1;
-				if (sum1 - minus1 > -1)score = score - a[sum1 - minus1];
+				//黑棋				
+				black(&sum1, &sum2, &minus1, &minus2, &score, temp, a);
 				//检查是否胜利
-				if (sum1 == 5) {
-					*temp = -10000000000000;
-					return;
-				}
-				else if (sum2 == 5) {
-					*temp = 10000000000000;
-					return;
-				}
-				sum1 = 0;
-				minus1 = 1;
+				if (checkwin(&sum1, &sum2,  temp))return;
+				blackback(&sum1, &sum2, &minus1, &minus2, &score, temp, a);
+
 			}
 			j++;
 		}
-		//检查最后边界
-		minus1 = minus1 + 1;
-		minus2 = minus2 + 1;
-		if (sum1 == 5) {
-			*temp = -10000000000000;
+		//抵达边界
+		minus1++;
+		minus2++;
+		if (checkwin(&sum1, &sum2, temp)) {
 			return;
 		}
-		else if (sum2 == 5) {
-			*temp = 10000000000000;
-			return;
-		}
-		else {
+		else {//结账
 			if (sum1 - minus1 > -1)score = score - 1.1 * a[sum1 - minus1];
 			if (sum2 - minus2 > -1)score = score + a[sum2 - minus2];
 		}
+
 		if (i != 0) {
+			initialize(&sum1, &sum2, &minus1, &minus2);
 			j = BOARD_LENTH - 1;
-			minus1 = 1;
-			minus2 = 1;
-			sum1 = 0;
-			sum2 = 0;
 			while (i + j >= 0 && j < BOARD_LENTH && j + i < BOARD_LENTH) {
 				if (board[j][i + j] == EMPTY) {
-					if (sum1 - minus1 > -1)score = score - 1.001 * a[sum1 - minus1];
-					if (sum2 - minus2 > -1)score = score + a[sum2 - minus2];
-					//检查是否胜利
-					if (sum1 == 5) {
-						*temp = -10000000000000;
-						return;
-					}
-					else if (sum2 == 5) {
-						*temp = 10000000000000;
-						return;
-					}
-					sum1 = 0;
-					sum2 = 0;
-					minus1 = 0;
-					minus2 = 0;
+					empty(&sum1, &sum2, &minus1, &minus2, &score, temp, a);
+					if (checkwin(&sum1, &sum2, temp))return;
+					emptyback(&sum1, &sum2, &minus1, &minus2, &score, temp, a);
 				}
-				else if (board[j][i + j] == 0) {
-					sum1 = sum1 + 1;
-					minus2 = minus2 + 1;
-					if (sum2 - minus2 > -1)score = score + a[sum2 - minus2];
+				else if (board[j][i + j] == WHITE) {
+					white(&sum1, &sum2, &minus1, &minus2, &score, temp, a);
+					//检查是否胜利
+					if (checkwin(&sum1, &sum2, temp))return;
+					whiteback(&sum1, &sum2, &minus1, &minus2, &score, temp, a);
 
-					//检查是否胜利
-					if (sum1 == 5) {
-						*temp = -10000000000000;
-						return;
-					}
-					else if (sum2 == 5) {
-						*temp = 10000000000000;
-						return;
-					}
-					sum2 = 0;
-					minus2 = 1;
 				}
-				else {
-					minus1 = minus1 + 1;
-					sum2 = sum2 + 1;
-					if (sum1 - minus1 > -1)score = score - 1.1 * a[sum1 - minus1];
+				else {//黑棋				
+					black(&sum1, &sum2, &minus1, &minus2, &score, temp, a);
 					//检查是否胜利
-					if (sum1 == 5) {
-						*temp = -10000000000000;
-						return;
-					}
-					else if (sum2 == 5) {
-						*temp = 10000000000000;
-						return;
-					}
-					sum1 = 0;
-					minus1 = 1;
+					if (checkwin(&sum1, &sum2,  temp))return;
+					blackback(&sum1, &sum2, &minus1, &minus2, &score, temp, a);
+
 				}
 				j--;
 			}
-			//检查最后边界
-			minus1 = minus1 + 1;
-			minus2 = minus2 + 1;
-			if (sum1 == 5) {
-				*temp = -10000000000000;
+			//抵达边界
+			minus1++;
+			minus2++;
+			if (checkwin(&sum1, &sum2, temp)) {
 				return;
 			}
-			else if (sum2 == 5) {
-				*temp = 10000000000000;
-				return;
-			}
-			else {
+			else {//结账
 				if (sum1 - minus1 > -1)score = score - 1.1 * a[sum1 - minus1];
 				if (sum2 - minus2 > -1)score = score + a[sum2 - minus2];
 			}
@@ -392,13 +276,6 @@ void evaluate(int** board,int user,int x,int y,int* temp) {
 }
 int search(int ** Board, int user, int times,int* temp,int extre) {
 	int max_score = -2147483647,save;
-	//int saveBoard[17][17];
-	////保存
-	//for (int i= 0; i < BOARD_LENTH; i++) {
-	//	for (int j = 0; j < BOARD_LENTH; j++) {
-	//		saveBoard[i][j] = Board[i][j];
-	//	}
-	//}
 
 
 	int t = *temp;
--- convention.h
+++ convention.h
@@ -3,6 +3,6 @@
 #define EMPTY 2
 #define BLACK 1
 #define WHITE 0
-#define DEPTH 6
+#define DEPTH 4
 #define KILLDEPTH 10
 int** InitializeBoard();
